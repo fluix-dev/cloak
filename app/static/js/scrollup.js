ac90800a9087d495c009747dc2127c57406dc59e
Justin Lu <wabasabi359@gmail.com>
2020-05-31 11:35:45 -0400

    add links to the landing and public pages

---
+++ app/static/js/scrollup.js
@@ -0,0 +1,11 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0,0);
+    document.documentElement.scrollTop(0, 0);
+window.scroll(0, 0);
+};
+
+function scrollTop(){
+    window.scrollTo(0,0);
+window.scroll(0, 0);
+    document.documentElement.scrollTop(0, 0);
+}

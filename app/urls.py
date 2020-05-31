c8b2009c67fd94e3cbc3c4b5c8d9e3228a9b86b9
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-05-31 02:23:40 -0400

    Add submission confirmation page

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("form/<uuid:uuid>/<str:form_id>/", views.fill, name="fill"),
     path("form/<uuid:uuid>/<str:form_id>/submit/", views.submit, name="submit"),
+    path("form/<uuid:uuid>/<str:form_id>/submitted/", views.submitted, name="submitted"),
 ]

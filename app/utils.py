8eb97e9cc47650673b5583bc979f4616b6a1520e
TheAvidDev <AvidDev@protonmail.com>
2020-05-30 22:04:12 -0400

    Increase default braille length

--- app/utils.py
+++ app/utils.py
@@ -3,5 +3,5 @@ import random
 BRAILLE_ALPHABET = "⣐⣑⣒⣓⣔⣕⣖⣗⣘⣙⣚⣛⣜⣝⣞⣟⣡⣢⣣⣤⣥⣦⣧⣨⣩⣪⣫⣬⣭⣮⣯⣱⣲⣳⣴⣵⣶⣷⣸⣹⣺⣻⣼⣽⣾⣿⣆⣇⣈⣉⣊⣋⣌⣍⣎⣏⢷⢸⢹⢺⢻⢼⢽⢾⢿⡳⡴⡵⡶⡷⡸⡹⡺⡻⡼⡽⡾⡿"
 
 
-def gen_braille(chars=6):
+def gen_braille(chars=8):
     return ''.join(random.choice(BRAILLE_ALPHABET) for _ in range(chars))
